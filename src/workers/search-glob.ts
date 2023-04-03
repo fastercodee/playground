6a81080465e3358b056977e9b30a0dcb9eb598fb
Tachibana Shin <tachibshin@duck.com>
2023-04-03 16:37:51 +0000

    [workers/search-glob]: only import `@capacitor/filesystem/web`
    ----
    The problem occurs because in the default compilation mode `iife` `vite` does not support `worker in worker` which leads to a compilation error
    ----

--- src/workers/search-glob.ts
+++ src/workers/search-glob.ts
@@ -1,22 +1,32 @@
 /* eslint-env webworker */
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference path="../../node_modules/typescript/lib/lib.webworker.d.ts" />
-import { Directory, Encoding, Filesystem } from "@capacitor/filesystem";
+import { Directory, Encoding } from "@capacitor/filesystem/dist/esm/definitions"
+import { FilesystemWeb } from "@capacitor/filesystem/dist/esm/web"
 import { listen, ping } from "@fcanvas/communicate"
 import { globby } from "src/logic/globby"
-import type { Match, SearchOptions } from "src/logic/search-text";
+import type { Match, SearchOptions } from "src/logic/search-text"
 import { searchText } from "src/logic/search-text"
-import { toTextFile } from "src/logic/to-text-file";
+import { toTextFile } from "src/logic/to-text-file"
 
-Object.assign(
-  self,
-  { window: self, Filesystem, Directory, Encoding },
-)
+const Filesystem = new FilesystemWeb()
+
+Object.assign(self, { window: self, Filesystem, Directory, Encoding })
 
 // eslint-disable-next-line @typescript-eslint/consistent-type-definitions, functional/no-mixed-types
 export type ComSearchGlob = {
-  "search-on-spa"(cwd: string, include: string[], exclude: string[], uid: string, searchOptions: SearchOptions): void
-  "search-in-text"(text: string, uid: string, searchOptions: SearchOptions): void
+  "search-on-spa"(
+    cwd: string,
+    include: string[],
+    exclude: string[],
+    uid: string,
+    searchOptions: SearchOptions
+  ): void
+  "search-in-text"(
+    text: string,
+    uid: string,
+    searchOptions: SearchOptions
+  ): void
 
   [name: `search-return-${string}`]: (opts: {
     file: null | string
@@ -24,44 +34,54 @@ export type ComSearchGlob = {
   }) => void
 }
 
-if (import.meta.env.MODE === "development" || import.meta.env.MODE === "spa" || import.meta.env.MODE === "pwa") {
-  listen<ComSearchGlob, "search-on-spa">(self, "search-on-spa", async (cwd, include, exclude, uid, searchOptions) => {
-    if (include.length === 0) {
-      // if empty default scan all files
-      include.push("**/*")
-    }
+if (
+  import.meta.env.MODE === "development" ||
+  import.meta.env.MODE === "spa" ||
+  import.meta.env.MODE === "pwa"
+) {
+  listen<ComSearchGlob, "search-on-spa">(
+    self,
+    "search-on-spa",
+    async (cwd, include, exclude, uid, searchOptions) => {
+      if (include.length === 0) {
+        // if empty default scan all files
+        include.push("**/*")
+      }
 
-    for await (
-      const file of await globby(cwd, include, exclude)
-    ) {
-      ping<ComSearchGlob, `search-return-${string}`>(self, `search-return-${uid}`, {
-        file,
-        matches: [
-          ...searchText(
-            await Filesystem.readFile({
-              path: file,
-              directory: Directory.External,
-              encoding: Encoding.UTF8
-            }).then(toTextFile),
-            searchOptions
-          )
-        ]
-      })
-    }
-  })
-} else {
-  listen<ComSearchGlob, "search-in-text">(self, "search-in-text", async (text, uid, searchOptions) => {
-    ping<ComSearchGlob, `search-return-${string}`>(self, `search-return-${uid}`, {
-      file: null,
-      matches: [
-        ...searchText(
-          text,
-          searchOptions
+      for await (const file of await globby(cwd, include, exclude)) {
+        ping<ComSearchGlob, `search-return-${string}`>(
+          self,
+          `search-return-${uid}`,
+          {
+            file,
+            matches: [
+              ...searchText(
+                await Filesystem.readFile({
+                  path: file,
+                  directory: Directory.External,
+                  encoding: Encoding.UTF8,
+                }).then(toTextFile),
+                searchOptions
+              ),
+            ],
+          }
         )
-      ]
-    })
-  })
+      }
+    }
+  )
+} else {
+  listen<ComSearchGlob, "search-in-text">(
+    self,
+    "search-in-text",
+    async (text, uid, searchOptions) => {
+      ping<ComSearchGlob, `search-return-${string}`>(
+        self,
+        `search-return-${uid}`,
+        {
+          file: null,
+          matches: [...searchText(text, searchOptions)],
+        }
+      )
+    }
+  )
 }
-
-
-export type { }
