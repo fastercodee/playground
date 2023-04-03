045610d60d30bd435c85dc38175fa5ba46b5cc55
Tachibana Shin <tachibshin@duck.com>
2023-04-03 14:48:48 +0000

    add css to editor

--- src/components/sketch/areas/Editor.vue
+++ src/components/sketch/areas/Editor.vue
@@ -30,4 +30,29 @@ watch(
 .parent-editor :deep(.cm-editor) {
   height: 100%;
 }
+
+.parent-editor {
+  // 0.25, 0.8
+  $bg-selection-match: #99eeff38;
+  $bg-matchingBracket: #626c8a40;
+  :deep(.cm-selectionBackground) {
+    // current section
+    background-color: $bg-selection-match;
+  }
+  :deep(.cm-activeLine) {
+    @apply bg-transparent relative;
+    &:before {
+      content: "";
+      @apply absolute w-full h-full top-0 left-0 z--1 border border-gray-400 border-opacity-20;
+    }
+  }
+  :deep(.cm-matchingBracket) {
+    // match braket
+    background-color: $bg-matchingBracket;
+  }
+  :deep(.cm-selectionMatch) {
+    // match equal section example <h1></h1> all "h1"
+    background-color: $bg-selection-match;
+  }
+}
 </style>
