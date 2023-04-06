f2be5a243c7611bee210c2dcd99ca1e9c003c775
Tachibana Shin <tachibshin@duck.com>
2023-04-06 12:39:39 +0000

    feat

--- src/components/sketch/SideBar/Search/logic/replace-fns.ts
+++ src/components/sketch/SideBar/Search/logic/replace-fns.ts
@@ -1,8 +1,17 @@
 import type { TreeDir } from "src/logic/flat-to-tree";
 import type { Match } from "src/logic/search-text";
 
+
+/**
+ * When edits are made then:
+ * - record file
+ * - update CodeMirror 6 doc if it's open
+ * Instead of splitting it into a logical file we should put it in the session-edit store
+ */
+
+
 export function replaceMatch(fullPath: string, match: Match) {
 
 }
-export function replaceMatches(fullPath: string, matches: Match[]) {}
-export function replaceMultiMatches(children: TreeDir["children"]) {}
+export function replaceMatches(fullPath: string, matches: Match[]) { }
+export function replaceMultiMatches(children: TreeDir["children"]) { }
