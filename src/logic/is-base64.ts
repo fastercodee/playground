32816b32d49cf75f4b730bfd19e7b166e07ba11b
Tachibana Shin <tachibshin@duck.com>
2023-04-11 03:53:59 +0000

    fix read 'utf8' like 'base64' error

--- src/logic/is-base64.ts
+++ src/logic/is-base64.ts
@@ -2,5 +2,18 @@ const base64regex =
   /^([0-9a-zA-Z+/]{4})*(([0-9a-zA-Z+/]{2}==)|([0-9a-zA-Z+/]{3}=))?$/
 
 export function isBase64(str: string) {
-  return base64regex.test(str)
+  if (base64regex.test(str)) {
+
+    try {
+      return btoa(atob(str)) === str;
+    }
+    catch (err) {
+      return false;
+    }
+
+  }
+
+  return false
 }
+
+// bar4
--- src/logic/to-text-file.ts
+++ src/logic/to-text-file.ts
@@ -1,6 +1,6 @@
 import type { ReadFileResult } from "@capacitor/filesystem"
 
 export function toTextFile({ data }: ReadFileResult): string {
-  if (isBase64(data)) return new TextDecoder().decode(base64ToUint8(data))
+  // if (isBase64(data)) return new TextDecoder().decode(base64ToUint8(data))
   return data
 }
