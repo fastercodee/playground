615af28dbf16713a2fb520d6a2b17cd06279eca1
Tachibana Shin <tachibshin@duck.com>
2023-04-16 14:50:29 +0000

    rm pach-cap-fs

--- quasar.config.js
+++ quasar.config.js
@@ -33,7 +33,7 @@ module.exports = configure(function (/* ctx */) {
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
     // https://v2.quasar.dev/quasar-cli-vite/boot-files
-    boot: ["axios", "unocss", "patch-cap-fs"],
+    boot: ["axios", "unocss"],
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#css
     css: ["app.scss"],
