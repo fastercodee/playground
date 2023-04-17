8018b51d620d6ac2e10e68b5fcbe9081aaff59e2
Tachibana Shin <45375496+tachibana-shin@users.noreply.github.com>
2023-04-17 21:56:47 +0700

    [sketch] watch files update hashes_client

--- src/stores/sketch.ts
+++ src/stores/sketch.ts
@@ -1,10 +1,13 @@
 /* eslint-disable camelcase */
 
 
+import { relative } from "path"
+
 import { put } from "@fcanvas/communicate"
 import { defineStore } from "pinia"
 import { api, post } from "src/boot/axios"
 import { isNative } from "src/constants"
+import { sha256File } from "src/logic/sha256-file"
 import type { SketchController } from "src/types/api/Controller/SketchController"
 import type { File } from "src/types/api/Models/File"
 import { getHashesClient } from "src/workers/get-hashes-client"
@@ -20,39 +23,50 @@ function exists(path: string) {
     .catch(() => false)
 }
 
-async function saveFile(root: string, file: File) {
-  if (file.data)
-    await Filesystem.writeFile({
-      path: `${root}/${file.filePath}`,
-      directory: Directory.External,
-      encoding: Encoding.UTF8,
-      data: file.data,
-    })
-  // need load
-  else
-    await Filesystem.writeFile({
-      path: `${root}/${file.filePath}`,
-      directory: Directory.External,
-      data: await api
+async function getFileFromServer(uid: number): Promise<Uint8Array> {
+  return await api
         .post(
           "/sketch/get_file",
           {
-            uid: file.uid,
+        uid,
           },
           {
             responseType: "arraybuffer",
           }
         )
         .then((res) => new Uint8Array(res.data))
-        .then(uint8ToBase64),
-    })
 }
+async function saveFileWithUID(path: string, uid: number) {
+  await Filesystem.writeFile({
+    path,
+    directory: Directory.External,
+    data: uint8ToBase64(await getFileFromServer(uid)),
+    recursive: true
+  })
+  eventBus.emit("writeFile", path)
+}
+
+async function saveFile(path: string, file: File) {
+  if (file.data !== undefined) {
+    await Filesystem.writeFile({
+      path,
+      directory: Directory.External,
+      encoding: Encoding.UTF8,
+      data: file.data,
+    })
+    eventBus.emit("writeFile", path)
+  }
+  // need load
+  else
+    await saveFileWithUID(path, file.uid)
+}
+
 
 export const useSketchStore = defineStore("sketch", () => {
   const uid_sketch_opening = ref<number>(-1)
   const rootのsketch = computed(() => `home/${uid_sketch_opening.value}`)
 
-  const hashes_serverのFile = useFile<Record<string, string>, true>(
+  const hashes_serverのFile = useFile<Record<string, { uid: number; hash: string }>, true>(
     computed(() => `${rootのsketch.value}/.changes/hashes_server`),
     "{}",
     true,
@@ -70,6 +84,9 @@ export const useSketchStore = defineStore("sketch", () => {
       set: JSON.stringify,
     }
   )
+  eventBus.watch(rootのsketch, async (タイプ, パス) => {
+    hashes_clientのFile.data[relative(rootのsketch.value, パス)] = await sha256File(パス)
+  }, { dir: true })
 
   async function actionNextOpenSketch(
     onProgress: (action: "load_file", filePath: string) => void
@@ -83,30 +100,22 @@ export const useSketchStore = defineStore("sketch", () => {
       hashes: entries_ashes_client.map((item) => item[1]),
     })
 
-    const hashes_server: Record<string, string> = {}
-    const hashes_client: Record<string, string> = {}
+    const hashes_server: typeof hashes_serverのFile.data = {}
     for (const [filePath, change] of Object.entries(res.data.file_changes)) {
       switch (change.type) {
         case "M":
+          // save to temporacy wait undo
           await hashes_serverのFile.ready
-          hashes_server[filePath] = change.file.hash
+          hashes_server[filePath] = { uid: change.file.uid, hash: change.file.hash }
           break
         case "U+":
           onProgress("load_file", change.file.filePath)
-          await saveFile(rootのsketch.value, change.file)
-          hashes_client[filePath] = change.file.hash
+          await saveFile(`${rootのsketch.value}/${change.file.filePath}`, change.file)
           break
         case "U":
           break
       }
     }
-
-    await hashes_serverのFile.ready
-    hashes_serverのFile.data = hashes_server
-    hashes_clientのFile.data = Object.assign(
-      hashes_clientのFile.data,
-      hashes_client
-    )
   }
   async function fetch(sketch_uid: number) {
     uid_sketch_opening.value = sketch_uid
