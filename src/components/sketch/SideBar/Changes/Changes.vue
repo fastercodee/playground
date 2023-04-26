89f1838183a31f9aace8a7b872f1290c35fb9631
Tachibana Shin <tachibshin@duck.com>
2023-04-26 09:44:07 +0000

    [changes]: ui

--- src/components/sketch/SideBar/Changes/Changes.vue
+++ src/components/sketch/SideBar/Changes/Changes.vue
@@ -1,10 +1,20 @@
 <template>
   <header class="py-2 px-3 text-12px">CHANGES</header>
-  <main class="min-h-0 h-full select-none">main</main>
+  <main class="min-h-0 h-full select-none">
+    <ChangeBaseMain
+      v-for="(status, fullPath) in sketchStore.変化"
+      :key="fullPath"
+      :opening="false"
+      type="file"
+      :fullpath="fullPath"
+      :status="status"
+    />
+  </main>
 </template>
 
 <script lang="ts" setup>
 const sketchStore = useSketchStore()
 
+const tmp = sketchStore.変化
 window.sketchStore = sketchStore
 </script>
---
+++ src/components/sketch/SideBar/Changes/components/ChangeBaseMain.vue
@@ -0,0 +1,71 @@
+<template>
+  <div
+    class="flex flex-nowrap items-center py-2px px-2 cursor-pointer hover:bg-[rgba(100,100,100,0.5)] transition ease duration-100 group"
+  >
+    <div class="flex flex-nowrap items-center flex-1 min-w-0">
+      <Icon
+        v-if="type === 'directory'"
+        :icon="`codicon:chevron-${opening ? 'down' : 'right'}`"
+        class="size-17px mr-2px"
+      />
+      <img
+        :src="
+          getIcon({
+            light: false,
+            isFolder: type === 'directory',
+            isOpen: opening,
+            filepath: fullpath,
+          })
+        "
+        class="size-[1.3em] mr-5px"
+      />
+      <span>{{ basename(fullpath) }}</span>
+      <small
+        v-if="pathdir && !modeTree"
+        class="text-gray-400 text-12px ml-1.5 self-end truncate"
+        >{{ pathdir }}</small
+      >
+    </div>
+
+    <div class="hidden group-hover:!block">
+      <Icon
+        icon="codicon:go-to-file"
+        class="w-1.2em h-1.2em mr-1"
+      />
+      <Icon
+        icon="codicon:redo"
+        :horizontalFlip="true"
+        class="w-1.2em h-1.2em mr-1"
+      />
+      <Icon
+        icon="codicon:add"
+        class="w-1.2em h-1.2em"
+      />
+    </div>
+
+    <span>{{ status }}</span>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { basename, dirname, relative } from "path"
+
+import { Icon } from "@iconify/vue"
+import getIcon from "src/assets/material-icon-theme/dist/getIcon"
+import type { Entry } from "src/logic/read-details"
+import { StatusChange } from "src/stores/sketch"
+
+const props = defineProps<{
+  opening: boolean
+  type: Entry<"file" | "directory">["type"]
+  fullpath: string
+  status: StatusChange
+  modeTree?: boolean
+}>()
+
+const sketchStore = useSketchStore()
+
+const pathdir = computed(() =>
+  relative(sketchStore.rootのsketch, dirname(props.fullpath))
+)
+</script>
