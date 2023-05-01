841efdabd45961f2ca6387142ebfab2d39d7793c
Tachibana Shin <45375496+tachibana-shin@users.noreply.github.com>
2023-05-01 20:57:34 +0700

    login oauth2

--- src/boot/auth.ts
+++ src/boot/auth.ts
@@ -1,4 +1,4 @@
-import { boot } from "quasar/wrappers";
+import { boot } from "quasar/wrappers"
 import { createAuth } from "vue-auth3"
 import authDriver from "vue-auth3/drivers/auth/bearer"
 
@@ -17,11 +17,11 @@ export default boot(({ app, router }) => {
       auth: authDriver,
       http: {
         request: api,
-      }
+      },
     },
 
     plugins: {
-      router
+      router,
     },
 
     registerData: {
@@ -43,3 +43,33 @@ export default boot(({ app, router }) => {
 
   app.use(auth)
 })
+
+export function loginWithGoogle() {
+  const oauth2Endpoint = "https://accounts.google.com/o/oauth2/v2/auth"
+
+  // Parameters to pass to OAuth 2.0 endpoint.
+  const params = {
+    client_id: process.env.OAUTH2_GOOGLE_CLIENT_ID,
+    redirect_uri: "http://localhost:8000",
+    response_type: "id_token",
+    scope: "email profile openid",
+    include_granted_scopes: "true",
+    state: "pass-through value",
+    nonce: "1",
+  }
+
+  window.open(oauth2Endpoint + "?" + new URLSearchParams(params).toString())
+}
+
+export function loginWithGithub() {
+  const oauth2Endpoint = "https://github.com/login/oauth/authorize"
+
+  // Parameters to pass to OAuth 2.0 endpoint.
+  const params = {
+    client_id: process.env.OAUTH2_GITHUB_CLIENT_ID,
+    redirect_uri: "http://localhost:8000",
+    scope: "read:user user:email",
+  }
+
+  window.open(oauth2Endpoint + "?" + new URLSearchParams(params).toString())
+}
