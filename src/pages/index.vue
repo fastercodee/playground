8b473951bebbeb59a7242eef588b20691fd729f0
Tachibana Shin <tachibshin@duck.com>
2023-05-09 11:29:50 +0000

    pre check

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -13,12 +13,45 @@ alias: "sketch/:uid(\\d+)?"
 </template>
 
 <script lang="ts" setup>
+import { AxiosError } from "axios"
+
 const route = useRoute()
+const $q = useQuasar()
 const sketchStore = useSketchStore()
 
-if (route.params.uid ? parseInt(route.params.uid as string) > 0 : false) {
-  sketchStore.fetch(parseInt(route.params.uid as string))
-} else {
-  sketchStore.fetch(-1, false)
-}
+const loading = ref(false)
+const notFound = ref(false)
+const error = ref<unknown | null>(null)
+
+// init
+watch(
+  () => (route.params.uid ? parseInt(route.params.uid as string) : null),
+  async (uid) => {
+    loading.value = true
+    notFound.value = false
+    error.value = null
+
+    try {
+      if (uid && uid > 0) {
+        await sketchStore.fetch(uid)
+      } else {
+        await sketchStore.fetch(uid ?? -1, false)
+      }
+    } catch (err) {
+      if (
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        (err as AxiosError<any> | void)?.response?.data.code ===
+        "sketch_not_found"
+      ) {
+        notFound.value = true
+        return
+      }
+
+      error.value = err
+    } finally {
+      loading.value = false
+    }
+  },
+  { immediate: true }
+)
 </script>
